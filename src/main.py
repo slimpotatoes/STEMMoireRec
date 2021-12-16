4e6405bb0e6fd94dc1a683c16e7f9f434955bdce
unknown <alexandre.pofelski@gmail.com>
2021-12-16 15:39:38 -0500

    updates

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import userinput
 import datastruct
+import view
 
 # simple test
 
 filename = 'example\Silicon.dm3'
 im = userinput.read_dm3(filename)
+view.imview(im.data)
 
 
 if __name__ == '__main__':
--- src/view.py
+++ src/view.py
@@ -1,3 +1,10 @@
 import numpy as np
-import data
+import matplotlib.pyplot as plt
+import datastruct
 
+def imview(image, cmap='gray', vmin=None, vmax=None):
+    fig = plt.figure(figsize=(6,6))
+    ax = fig.add_subplot(111)
+    ax.imshow(image, cmap=cmap, vmin=vmin, vmax=vmax)
+    plt.show()
+    return 
